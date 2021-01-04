5780218bcbc8bbdcb5747bac7d4528c4f758def3
Tosif khan <43736520+beingkarnage@users.noreply.github.com>
2021-01-04 19:39:50 +0530

    Add files via upload

---
+++ urlshortner/urls.py
@@ -0,0 +1,35 @@
+"""urlshortner URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from shortner.views import *
+
+urlpatterns = [
+    path('a/', admin.site.urls),
+    path('home/',home,name='home'),
+    path('login/',Userlogin,name='loginU'),
+    path('register/',register , name='register'),
+    path('getUrl',getUrl,name='getUrl'),
+    path('url/<slug:url>',UrlDriver),
+    path('logout',logoutUser,name='logoutU'),
+
+    # path(demo(),demo,name='demo'),
+
+    path('verifyEmail/<str:pk>',verifyEmail, name='verify'),
+    path('changePassword',change_password,name="changepass"),
+    path('check_username_exist',check_username_exist,name='check_username_exist'),
+]
